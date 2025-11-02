8dc4e07ec034ce457a3d0e447eda16a2cf19dde6
Christian Borss <christian@borss.name>
2025-11-02 12:56:01 +0100

    updated package-lock.json and dist files

---
+++ dist/assets/__vite-browser-external-legacy-FN4gk1CA.js
@@ -0,0 +1,1 @@
+System.register([],function(e,t){"use strict";return{execute:function(){var t=Object.freeze(Object.defineProperty({__proto__:null,default:{}},Symbol.toStringTag,{value:"Module"}));e("_",t)}}});
